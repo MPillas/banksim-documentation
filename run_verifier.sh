b2db7a78f9b6ee851885916a73d712e30e365ff1
MPillas <73175012+MPillas@users.noreply.github.com>
2023-10-05 11:04:00 +0200

    Add files via upload

---
+++ run_verifier.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Remember to activate a conda environment! Some common choices are:"
+echo "conda deactivate; conda activate /home/francesco.pannarale/.conda/envs/pygrb_devel"
+echo "conda deactivate; conda activate /home/lorenzo.piccari/.conda/envs/pycbc_tesi"
+echo
+
+pycbc_make_bank_verifier_workflow --config-files global_setup.ini --workflow-name bank_verificator --output-dir /home/marion.pillas/projects/template_bank/O3PSD/output_heavy_BBH_fstart5/ --submit-now
