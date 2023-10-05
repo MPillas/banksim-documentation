39553ff6551b23d809fe97692bb9771cfb731d30
MPillas <73175012+MPillas@users.noreply.github.com>
2023-10-05 11:04:38 +0200

    Update run_verifier.sh

--- run_verifier.sh
+++ run_verifier.sh
@@ -5,4 +5,4 @@ echo "conda deactivate; conda activate /home/francesco.pannarale/.conda/envs/pyg
 echo "conda deactivate; conda activate /home/lorenzo.piccari/.conda/envs/pycbc_tesi"
 echo
 
-pycbc_make_bank_verifier_workflow --config-files global_setup.ini --workflow-name bank_verificator --output-dir /home/marion.pillas/projects/template_bank/O3PSD/output_heavy_BBH_fstart5/ --submit-now
+pycbc_make_bank_verifier_workflow --config-files global_setup.ini --workflow-name bank_verificator --output-dir /PATH/TO/OUTPUT/DIRECTORY --submit-now
